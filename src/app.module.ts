f871074ca355076471677e0694ccb0ac6527e20c
anne.auzier <anne.auziier@outlook.com>
2026-07-16 16:24:03 -0400

    feat: task module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TaskController } from './task/task.controller';
+import { TaskModule } from './task/task.module';
+import { TaskService } from './task/task.service';
 
 @Module({
-  imports: [AuthModule, 
-    UserModule, 
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    DatabaseModule],
-  controllers: [], 
-  providers: [],
+  imports: [
+    AuthModule, 
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TaskModule
+  ],
+  controllers: [AppController], 
+  providers: [AppService],
 })
 export class AppModule {}
